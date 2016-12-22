4569f68ba2179d4f5d3aab0347f9f776c09dedbf
Gurpreet <gurpreetsk@hotmail.com>
2016-12-22 07:56:49 +0530

    feat: moved to external templates

--- my_site/urls.py
+++ my_site/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from my_site.views import hello, return_date, added_time
+from my_site.views import hello, return_date, added_time, template_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hello/$', hello),  # /hello/ should be the exact value, as in www. google.com/hello
     url(r'^datetime/$', return_date),
-    url(r'^datetime/plus/(\d{1,5})/$', added_time)  # value within () is offset
+    url(r'^datetime/plus/(\d{1,5})/$', added_time),  # value within () is offset
+    url(r'^template/$', template_view)
     # url(r'^$', hello)       # matches for root value
 ]
--- my_site/views.py
+++ my_site/views.py
@@ -1,14 +1,20 @@
 from django.http import HttpResponse, Http404
 import datetime
+from django import template
+from django.template.loader import get_template
 
 
 def hello(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("Hello World")
 
 
 def return_date(request):
-    time = "The time is %s." % datetime.datetime.now()  # string formatting, datetime object converted to string by %s
-    return HttpResponse(time)
+    # time = "The time is %s." % datetime.datetime.now()  # string formatting, datetime object converted to string by %s
+    # templates should be stored in templates folder
+    temp = get_template('return_date_template.html')
+    context = template.Context({"time_now": datetime.datetime.now()})
+    html = temp.render(context)
+    return HttpResponse(html)
 
 
 def added_time(request, offset):
@@ -19,3 +25,9 @@ def added_time(request, offset):
     future_time = datetime.datetime.now() + datetime.timedelta(hours=offset)
     statement = "The time in %s hour(s) will be %s" % (offset, future_time)
     return HttpResponse(statement)
+
+
+def template_view(request):
+    t = template.Template("My name is {{name}}.")
+    context = template.Context({'name': "Gurpreet"})
+    t.render(context)
