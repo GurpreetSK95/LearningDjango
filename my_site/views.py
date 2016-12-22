31e3a398898afe8e300cc44829cf36889b259346
Gurpreet <gurpreetsk@hotmail.com>
2016-12-22 08:50:30 +0530

    - including external templates in another html file

--- my_site/views.py
+++ my_site/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse, Http404
 import datetime
-from django import template
-from django.template.loader import get_template
+# from django import template
+# from django.template.loader import get_template
+from django.template import Context
+from django.shortcuts import render
 
 
 def hello(request):
@@ -11,10 +13,13 @@ def hello(request):
 def return_date(request):
     # time = "The time is %s." % datetime.datetime.now()  # string formatting, datetime object converted to string by %s
     # templates should be stored in templates folder
-    temp = get_template('return_date_template.html')
-    context = template.Context({"time_now": datetime.datetime.now()})
-    html = temp.render(context)
-    return HttpResponse(html)
+    # temp = get_template('return_date_template.html')  # give absolute template path
+    context = Context({"time_now": datetime.datetime.now(), "test": "test-test"})
+    # html = temp.render(context)
+    # return HttpResponse(html)
+
+    # all the above code is replaced using render function
+    return render(request, 'return_date_template.html', context)    # shortcut to return template
 
 
 def added_time(request, offset):
