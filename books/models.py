84509d2e99cd8a6d1937b0d11c7d2b83917711ad
Gurpreet <gurpreetsk@hotmail.com>
2016-12-22 15:09:25 +0530

    topic: django models
    - created app books
    - created models and notes file

--- books/models.py
+++ books/models.py
@@ -10,11 +10,17 @@ class Publisher(models.Model):
     country = models.CharField(max_length=20)
     website = models.URLField()
 
+    def __str__(self):          # to return string representation, MUST RETURN ONLY STRING, ELSE TypeError
+        return self.name
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
+
+    def __str__(self):
+        return u'%s %s' %(self.first_name, self.last_name)
 
 
 class Book(models.Model):
@@ -22,3 +28,6 @@ class Book(models.Model):
     author = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
